d4c1fd23287b790b46b0b914ff8b30f3154fd8ef
Alexmalinskiy <ivanov@example.comalexmalinskiy@gmail.com>
2017-06-09 20:23:28 +0400

    add function as,m

--- main_4_lecture.py
+++ main_4_lecture.py
@@ -8,47 +8,54 @@ def main():
         '2': ['10006'],
         '3': []}
     while True:
-        print("Введите команду. Для выхода наберите q. Список всех команд - help")
-        inp = input()
-        if inp.lower() == "q":
+        inp = input_data("Введите команду. Для выхода наберите q. Список всех команд - help",False)
+        if str(inp).lower() == "q":
             print("Сеанс завершён.")
+            print("На выходе документы имеют вид:", documents)
+            print("На выходе полки имеют вид:", directories)
+
             break
-        elif inp.lower() == "help": 
+        
+        elif str(inp).lower() == "help": 
             list_all_commands()
             
-        elif inp.lower() == "p":
-            print("Введите номер документа")
-            num = input()        
-            if num.isalpha():
+        elif str(inp).lower() == "p":
+            num = input_data("Введите номер документа",True)        
+            if num == None:
                 print("Неверно введен номер документа")
+                exit()
             Man = get_person_by_doc_number(documents, num)
             check_return(Man)
             
-        elif inp.lower() == "l":
+        elif str(inp).lower() == "l":
             list_all_docs(documents)
             
-        elif inp.lower() == "s":
-            print("Введите номер документа")
-            num = input()
-            if num.isalpha():
-                print("Неверно введен номер документа")           
+        elif str(inp).lower() == "s":
+            num = input_data("Введите номер документа",True)        
+            if num == None:
+                print("Неверно введен номер документа")
+                exit()          
             key = get_shelf_by_doc_number(directories, num)
             check_return(key)
             
-        elif inp.lower() == "a":
+        elif str(inp).lower() == "a":
             documents, directories = add_doc(documents, directories)
             
-        elif inp.lower() == "d":
-            print("Введите номер документа")
-            num = input()
-            if num.isalpha():
-                print("Неверно введен номер документа")    
+        elif str(inp).lower() == "d":
+            num = input_data("Введите номер документа",True)        
+            if num == None:
+                print("Неверно введен номер документа")
+                exit()  
             documents, directories = remove_doc_by_number(documents, directories, num)
+        elif str(inp).lower() == "m":
+            directories = move_doc(documents,directories)
             
-##        elif inp.lower() == "m":
-##
-##        elif inp.lower() == "as":
-##
+        elif str(inp).lower() == "as":
+            shelf_num = input_data("Введите номер полки",True)
+            if shelf_num == None:
+                print("Неверно введён номер полки")
+                exit()
+            directories = add_directory(directories,shelf_num)
         else:
             print("Неверная команда!")
 
@@ -92,31 +99,50 @@ def get_shelf_by_doc_number(directories, num):
         if num in item:
             return key
 
+def input_data(text, isdigit):
+# функция ввода данных. передаем признак isdigit, если true значит должно принимать только числа, если false то буквы и числа
+    print(text)
+    inp = input()
+    if isdigit == True:
+        if inp.isalpha():
+            return
+    else:
+        if inp.isdigit():
+            return
+    return inp
+    
+def add_directory(directories,shelf_num):
+# добавляет полку
+    Found_shelf = False
+    for key,item in directories.items():
+        if key == shelf_num:
+            Found_shelf = True
+            print("Такая полка уже есть")
+    if Found_shelf == False:        
+        directories[shelf_num] = []
+    return directories
+
 def add_doc(documents,directories):
 # добавить документ на полку
     found = False # нашли ли полку по номеру
     
-    print("Введите тип документа")
-    type = input()
-    if type.isdigit():
+    type = input_data("Введите тип документа",False)        
+    if type == None:
         print("Неверно введён тип документа")
         return documents, directories
     
-    print("Введите номер документа")
-    num = input()
-    if num.isalpha():
+    num = input_data("Введите номер документа", True)
+    if num == None:
         print("Неверно введён номер документа")
         return documents, directories
-    
-    print("Введите имя")
-    name = input()
-    if name.isdigit():
+
+    name = input_data("Введите имя", False)
+    if name == None:
         print("Неверно введёно имя владельца")
         return documents, directories
     
-    print("Введите номер полки")
-    shelf_num = input()
-    if shelf_num.isalpha():
+    shelf_num = input_data("Введите номер полки",True)
+    if shelf_num == None:
         print("Неверно введён номер полки")
         return documents, directories
     
@@ -129,6 +155,38 @@ def add_doc(documents,directories):
     if found == False:
         print("Полка не найдена!")
     return documents, directories
+
+def move_doc(documents,directories):
+# передвигает документ
+    num = input_data("Введите номер документа",True)        
+    if num == None:
+        print("Неверно введен номер документа")
+        exit()
+
+    shelf_num = input_data("Введите номер полки",True)
+    if shelf_num == None:
+        print("Неверно введён номер полки")
+        exit()
+
+    Found_doc = False
+    Found_shelf = False
+    if get_shelf_by_doc_number(directories, num) != None:
+        old_shelf_num = get_shelf_by_doc_number(directories, num)
+        Found_doc = True
+
+    if Found_doc == True:
+        for key,item in directories.items():
+            if key == shelf_num:
+                Found_shelf = True
+                directories[old_shelf_num].remove(num)
+                item.append(num)
+                break
+        if Found_shelf == False:
+            print("Полка не найдена!")
+    else:
+        print("Документ не найден")
+    return directories    
+    
 
 def remove_doc_by_number(documents,directories,num):
 # удаляет документ из списков по номеру
